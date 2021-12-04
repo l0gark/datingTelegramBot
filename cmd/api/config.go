3c2b154bba6599282599eb6e815936053efce471
Eretic431 <abdullapower@outlook.com>
2021-12-04 22:12:31 +0300

    added docker targets to makefile

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -5,7 +5,7 @@ import "github.com/caarlos0/env"
 type config struct {
 	Production  bool   `env:"PRODUCTION" envDefault:"false"`
 	Port        string `env:"PORT" envDefault:"80"`
-	PostgresUrl string `env:"POSTGRES_URL" envDefault:"postgres://postgres:tutorpass@db:5432/postgres?sslmode=disable"`
+	PostgresUrl string `env:"POSTGRES_URL" envDefault:"postgres://postgres:tgbot@db:5432/postgres?sslmode=disable"`
 	TgBotToken  string `env:"BOT_TOKEN" envDefault:"none"`
 }
 

2907271d87661efb0e015aef19534ab289ba7a97
l0gark <l0gark@ya.ru>
2021-12-05 23:27:17 +0300

    make like id autoincrement int64 value

--- internal/data/models/like.go
+++ internal/data/models/like.go
@@ -2,7 +2,7 @@ package models
 
 // Like model
 type Like struct {
-	Id     string `db:"id"`
+	Id     int64  `db:"id"`
 	FromId string `db:"from_id"`
 	ToId   string `db:"to_id"`
 	Showed bool   `db:"showed"`
--- migrations/000002_create_likes_table.up.sql
+++ migrations/000002_create_likes_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS likes
 (
-    id      varchar PRIMARY KEY NOT NULL,
-    from_id varchar             NOT NULL REFERENCES users (id),
-    to_id   varchar             NOT NULL REFERENCES users (id),
-    showed  bool                NOT NULL
+    id      bigserial PRIMARY KEY NOT NULL,
+    from_id varchar               NOT NULL REFERENCES users (id),
+    to_id   varchar               NOT NULL REFERENCES users (id),
+    showed  bool                  NOT NULL
 );

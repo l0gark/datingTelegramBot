73dfbbefc8f4d4b18c5e15aa308c24c60ada32f9
l0gark <l0gark@ya.ru>
2021-12-05 23:11:37 +0300

    create models and db entities

--- internal/data/models/user.go
+++ internal/data/models/user.go
@@ -2,5 +2,11 @@ package models
 
 // User model
 type User struct {
-	Id string `db:"id"`
+	Id          string `db:"id"`
+	Name        string `db:"name"`
+	Sex         string `db:"sex"`
+	Age         int    `db:"age"`
+	Description string `db:"description"`
+	City        string `db:"city"`
+	Image       string `db:"image"`
 }
--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,3 +1,10 @@
-CREATE TABLE IF NOT EXISTS users(
-  id varchar PRIMARY KEY
+CREATE TABLE IF NOT EXISTS users
+(
+    id          varchar PRIMARY KEY NOT NULL,
+    name        varchar             NOT NULL,
+    sex         varchar(6),
+    age         int                 NOT NULL,
+    description text                NOT NULL,
+    city        text                NOT NULL,
+    image       text                NOT NULL
 );

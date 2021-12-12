4269bdf4f0fed877a1bd1b09211a385b890aff6d
l0gark <potapova.ma@mail.ru>
2021-12-12 18:34:08 +0300

    add mocked command handlers

--- cmd/api/bot.go
+++ cmd/api/bot.go
@@ -11,11 +11,58 @@ func (a *application) handleUpdates() {
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		var outputMsg *tgbotapi.MessageConfig
 
-		a.bot.Send(msg)
+		switch update.Message.Text {
+		case "/start":
+			outputMsg = a.handleCommandStart(update.Message)
+		case "/profile":
+			outputMsg = a.handleCommandProfile(update.Message)
+		case "/next":
+			outputMsg = a.handleCommandNext(update.Message)
+		default:
+			outputMsg = a.handleUndefinedMessage(update.Message)
+		}
+
+		if outputMsg != nil {
+			if _, err := a.bot.Send(outputMsg); err != nil {
+				a.log.Warnf("could not send message with error %e", err)
+				return
+			}
+		}
 	}
+}
+
+func (a *application) handleCommandStart(inputMsg *tgbotapi.Message) *tgbotapi.MessageConfig {
+	a.log.Info("handleCommandStart")
+	outputMsg := tgbotapi.NewMessage(inputMsg.Chat.ID, "Hello")
+	return &outputMsg
+}
+
+func (a *application) handleCommandProfile(inputMsg *tgbotapi.Message) *tgbotapi.MessageConfig {
+	a.log.Info("handleCommandProfile")
+	outputMsg := tgbotapi.NewMessage(inputMsg.Chat.ID, "Hello")
+	return &outputMsg
+}
+
+func (a *application) handleCommandNext(inputMsg *tgbotapi.Message) *tgbotapi.MessageConfig {
+	a.log.Info("handleCommandNext")
+	outputMsg := tgbotapi.NewMessage(inputMsg.Chat.ID, "Hello")
+	return &outputMsg
+}
+
+func (a *application) handleUndefinedMessage(inputMsg *tgbotapi.Message) *tgbotapi.MessageConfig {
+	a.log.Info("handleUndefinedMessage")
+	outputMsg := tgbotapi.NewMessage(inputMsg.Chat.ID, "Такой команды не существует.\n\n"+
+		"*Список доступных команд:* \n"+
+		"- /start - начало работы\n"+
+		"- /profile - заполнить анкету\n"+
+		"- /next - показать следующего пользователя",
+	)
+	outputMsg.ParseMode = tgbotapi.ModeMarkdown
+
+	return &outputMsg
+
 }
 
 func newTgBot(c *config) (*tgbotapi.BotAPI, error) {

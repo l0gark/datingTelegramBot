f0cf9b0c26a18bd2839946e901ac9d3afd82cc24
l0gark <l0gark@ya.ru>
2021-12-14 15:23:06 +0300

    fix F_Next

---
+++ migrations/000003_insert_users.up.sql
@@ -0,0 +1,32 @@
+/* MEN */
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('pavelHi', 'Павел', true, 24, 'Здравствуйте, я Павел', 'Санкт-Петербург', 'AgACAgIAAxkBAAIE8mG4VIRk5OJKBtKYqW00gkyCs_3NAAJStjEbfhrJSdVb8sIYFC8YAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('yura521', 'Юрий', true, 21, 'Смотрю футбол', 'Москва', 'AgACAgIAAxkBAAIE9GG4VMq5XTuHcOoLDnv8glrFsqaWAAJTtjEbfhrJSQQ7SqC2QPoQAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('vanya123', 'Иван', true, 28, 'Люблю фильмы про кофе', 'Самара', 'AgACAgIAAxkBAAIE9mG4VM1TsMix_1lXU-uxjYvufy2UAAJUtjEbfhrJSayBDO8b1LpQAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('mishOK', 'Михаил', true, 32, 'Я программист', 'Нальчик', 'AgACAgIAAxkBAAIE-GG4VNF3TLIWfSkNHUR8XbvRWQVyAAJVtjEbfhrJSTnb0U9FI-rLAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('dimon_off', 'Дмитрий', true, 16, 'Весёлый, красивый и умный. (с) Моя бабушка', 'Ростов', 'AgACAgIAAxkBAAIE-mG4VNSVpXncBM33M1pj-LrwNNpeAAJWtjEbfhrJSbrW1dkSpwlSAQADAgADcwADIwQ', 0, -1, true);
+
+/* WOMEN */
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('mashunya', 'Мария', false, 21, 'Я самая счастливая', 'Санкт-Петербург', 'AgACAgIAAxkBAAIE_GG4VTS3J7YV2iqzbZQqfIlfWwyrAAJXtjEbfhrJSeF1UfAZe7JfAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('pavlovna', 'Дарья', false, 24, 'Обожаю кушать.', 'Москва', 'AgACAgIAAxkBAAIE_mG4VTkZlxzx1l-3RkRUhWnTIChHAAJYtjEbfhrJSeUm1qZWdvxdAQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('alexandro', 'Александра', false, 35, 'Мне нужен мужчина для серьезных отношений', 'Самара', 'AgACAgIAAxkBAAIFAAFhuFU8E8K5qrMrpXR2JILHalC2-QACWbYxG34ayUl7g9MBl59wjQEAAwIAA3MAAyME', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('jiliy', 'Юлия', false, 22, 'Я программистка', 'Нальчик', 'AgACAgIAAxkBAAIFAmG4VUBYMGSIePC0P6-j9HsiPZYlAAJatjEbfhrJSUnfhaXF7i25AQADAgADcwADIwQ', 0, -1, true);
+
+INSERT INTO users (id, name, sex, age, description, city, image, chat_id, stage, started)
+VALUES ('asya', 'Ася', false, 15, 'Я скоро буду сдавать ОГЭ, поэтому гулять некогда...', 'Ростов', 'AgACAgIAAxkBAAIFBGG4VURfyPtjNk2FZ7yQUNuDr2qIAAJbtjEbfhrJSddHf3GkVt-VAQADAgADcwADIwQ', 0, -1, true);
+

663bec519ef7c865e05f93238ee99641e6b9f6c4
Abdulla Akhmedov <akhmedov.a@selectel.ru>
2022-04-15 23:10:47 +0300

    fix

--- cmd/api/bot.go
+++ cmd/api/bot.go
@@ -49,7 +49,6 @@ func (a *application) handleUpdates() {
 
 func (a *application) handleMessage(ctx context.Context, msg *tgbotapi.Message) ([]tgbotapi.Chattable, error) {
 	user, err := a.users.GetByUserId(ctx, msg.From.UserName)
-	log.Print(user.Stage)
 	if err != nil && !errors.Is(err, models.ErrNoRecord) {
 		return nil, err
 	}

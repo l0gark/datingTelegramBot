ee7e60de8637992e29150ed46d453cfbc10b7400
Abdulla Akhmedov <akhmedov.a@selectel.ru>
2022-04-15 20:43:20 +0300

    fix

--- cmd/api/integration_test.go
+++ cmd/api/integration_test.go
@@ -13,7 +13,7 @@ import (
 func Test_Scenario1_1(t *testing.T) {
 	app := newTestApp()
 
-	msg := &tgbotapi.Message{From: &tgbotapi.User{UserName: "test"}, Text: "/next"}
+	msg := &tgbotapi.Message{From: &tgbotapi.User{UserName: "test"}, Text: "@/start"}
 	ctx := context.Background()
 	chattable, _ := app.handleMessage(ctx, msg)
 	resp := chattable[0].(*tgbotapi.MessageConfig)
